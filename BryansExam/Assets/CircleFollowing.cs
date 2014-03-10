aed9d9bd87ece2951d7e339d5b2edb94fc3d6e9b
Jamie O Flanagan <jamie@jamiegamedev.com>
2014-03-10 18:47:58 +0000

    first commit
    commit one

---
+++ BryansExam/Assets/CircleFollowing.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleFollowing : MonoBehaviour {
+	public float mass;
+	public float maxSpeed;
+	public float velocity;
+	public float force;
+	public int currentWaypoint;
+	public ArrayList waypoints;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	private void CreatePath(int points, float radius)
+	{
+
+	
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ BryansExam/Assets/seekAndArrive.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//This is about the best I can do at the moment. I got the cube follwing an object. Random positions etc where a bit above my pay grade.
+
+
+public class seekAndArrive : MonoBehaviour {
+
+	public Vector3 velocity = Vector3.zero;
+	public Vector3 ForceAcc = Vector3.zero;
+	public float mass;
+	//public float maxspeed = 10.0f;
+	public GameObject enemy;
+	//public Vector3 desiredTarget = new Vector3(0,0,0);
+	
+	Vector3 Seek(Vector3 target)
+	{
+		Vector3 desired = target - transform.position;
+		desired.Normalize();
+		//desired*= maxspeed;
+		return desired - velocity;
+		
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		mass = 1.0f;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//float forceAmount = 10.0f;
+		//maxspeed = 30.0f;
+		//ForceAcc += Seek(desiredTarget);
+		//float distance = Vector3.Distance(transform.position,enemy.transform.position);
+		ForceAcc += Seek(enemy.transform.position);
+		
+		Vector3 accel = ForceAcc / mass;
+		velocity = velocity + accel * Time.deltaTime;
+		transform.position = transform.position + velocity * Time.deltaTime;
+		ForceAcc = Vector3.zero;
+		
+		if(velocity.magnitude <= float.Epsilon)
+		{
+			transform.forward = Vector3.Normalize(velocity);
+		}
+	}
+}
